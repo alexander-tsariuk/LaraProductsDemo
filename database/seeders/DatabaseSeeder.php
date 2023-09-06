3453522b65f1c1b12f01ba9a6ab05e739285353b
alexander <alexander.tsariuk@icloud.com>
2023-09-06 17:38:29 +0300

    first

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use App\Models\Warehouse;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,24 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Warehouse::insert([
+            'name' => 'Склад 1'
+        ], [
+            'name' => 'Склад 2'
+        ], [
+            'name' => 'Склад 3'
+        ]);
+
+
+        Product::insert([
+            'name' => 'Товар 1',
+            'article' => 'tovar1',
+            'price' => 10.5
+        ], [
+            'name' => 'Товар 2',
+            'article' => 'tovar2',
+            'price' => 11.2
+        ]);
     }
 }
